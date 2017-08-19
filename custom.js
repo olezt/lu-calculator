936260570895649a838920a4cbd0094800f7ea48
olezt <lfants666@gmail.com>
2017-08-19 15:49:35 +0300

    Lu now actually barks

--- custom.js
+++ custom.js
@@ -3,6 +3,12 @@ function lunaTalk(delay, times){
 		$("#lunaUp").toggleClass('talkUp');
 		$("#lunaDown").toggleClass('talkDown');
 	}, delay, times);
+	
+	var audio = new Audio('./sound/bark.mp3');
+	setIntervalX(function () {
+		audio.play();
+	}, delay*2, times/2);
+	
 }
 
 function setIntervalX(callback, delay, repetitions) {
